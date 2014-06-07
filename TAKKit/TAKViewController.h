afbb2af860ef9c4ad15a499fcc0f70b26b60e307
Takahiro Ooishi <taka0125@gmail.com>
2014-06-08 04:27:56 +0900

    add project

---
+++ TAKKit/TAKViewController.h
@@ -0,0 +1,13 @@
+//
+//  TAKViewController.h
+//  TAKKit
+//
+//  Created by Takahiro Ooishi on 2014/06/08.
+//  Copyright (c) 2014年 Takahiro Ooishi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TAKViewController : UIViewController
+
+@end
